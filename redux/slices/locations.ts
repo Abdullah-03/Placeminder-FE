0021e48dec6e00ba316bd1e48a02affe1e6e2916
Abdullah Saud <abdullahsaud03@gmail.com>
2024-09-26 18:18:10 +0500

    Resolve feedback

--- redux/slices/locations.ts
+++ redux/slices/locations.ts
@@ -1,4 +1,4 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import {createSlice, PayloadAction} from '@reduxjs/toolkit';
 
 interface LocationInterface {
     name: string;
@@ -6,83 +6,115 @@ interface LocationInterface {
     radius: number;
     tasks: string[];
 }
+
 export interface LocationsInterface {
     locations: LocationInterface[];
     error: string | null;
 }
 
 const initialState: LocationsInterface = {
-    locations: [
-        {
-            name: '',
-            coordinates: '',
-            radius: 1,
-            tasks: [],
-        },
-    ],
+    locations: [],
     error: null,
 };
+
+function findLocation(locations: LocationInterface[], locationName: string): [number, string | null] {
+    const index = locations.findIndex(location => location.name === locationName);
+    return index !== -1 ? [index, null] : [-1, 'Location does not exist'];
+}
+
+function findTask(locations: LocationInterface[], locationName: string, taskName: string): [number, number | null, string | null] {
+    const locationIndex = locations.findIndex(location => location.name === locationName);
+    if (locationIndex === -1) return [-1, null, 'Location does not exist'];
+
+    const taskIndex = locations[locationIndex].tasks.indexOf(taskName);
+    if (taskIndex === -1) return [locationIndex, -1, 'Task does not exist'];
+
+    return [locationIndex, taskIndex, null];
+}
 
 export const locationsSlice = createSlice({
     name: 'locations',
     initialState,
     reducers: {
         addLocation: (state, action: PayloadAction<LocationInterface>) => {
-            const exists = state.locations.some((location) => location.name === action.payload.name);
-            if (!exists) {
+            const [locationIndex] = findLocation(state.locations, action.payload.name);
+            if (locationIndex !== -1) {
+                state.error = 'Location with this name already exists.';
+            } else {
                 state.locations.push(action.payload);
                 state.error = null;
-            } else {
-                state.error = 'Location with this name already exists.';
             }
         },
         removeLocation: (state, action: PayloadAction<string>) => {
-            state.locations = state.locations.filter((location) => location.name !== action.payload);
-            state.error = null;
-        },
-        updateLocation: (state, action: PayloadAction<string>) => {
-            const i = state.locations.findIndex((l) => l.name === action.payload);
-            if (i !== -1) {
-                state.locations[i].name = action.payload;
-                state.error = null;
+            const [locationIndex, error] = findLocation(state.locations, action.payload);
+            if (error) {
+                state.error = error;
             } else {
-                state.error = 'Given location name does not exist';
+                state.locations.splice(locationIndex, 1);
+                state.error = null;
+            }
+        },
+        updateLocation: (state, action: PayloadAction<{
+            prevLocation: LocationInterface,
+            newLocation: LocationInterface
+        }>) => {
+            const [locationIndex, error] = findLocation(state.locations, action.payload.prevLocation.name);
+            if (error) {
+                state.error = error;
+            } else {
+                state.locations[locationIndex] = action.payload.newLocation;
+                state.error = null;
             }
         },
 
         addTask: (state, action: PayloadAction<{ locationName: string; taskName: string }>) => {
-            const i = state.locations.findIndex((l) => l.name === action.payload.locationName);
-            if (i !== -1) {
-                const j = state.locations[i].tasks.findIndex((t) => t === action.payload.taskName);
-                if (j === -1) {
-                    state.locations[i].tasks.push(action.payload.taskName);
-                    state.error = null;
-                } else {
-                    state.error = 'Given task already exists';
-                }
+            const [locationIndex, taskIndex] = findTask(state.locations, action.payload.locationName, action.payload.taskName);
+            if (locationIndex === -1) {
+                state.error = 'Location does not exist';
+            } else if (taskIndex !== -1) {
+                state.error = 'Task already exists';
             } else {
-                state.error = 'Given location name does not exist';
+                state.locations[locationIndex].tasks.push(action.payload.taskName);
+                state.error = null;
             }
         },
         removeTask: (state, action: PayloadAction<{ locationName: string; taskName: string }>) => {
-            const i = state.locations.findIndex((l) => l.name === action.payload.locationName);
-            if (i !== -1) {
-                const j = state.locations[i].tasks.findIndex((t) => t === action.payload.taskName);
-                if (j !== -1) {
-                    state.locations[i].tasks.splice(j, 1);
-                    state.error = null;
-                } else {
-                    state.error = 'Given task does not exist';
-                }
+            const [locationIndex, taskIndex, error] = findTask(state.locations, action.payload.locationName, action.payload.taskName);
+            if (error) {
+                state.error = error;
             } else {
-                state.error = 'Given location name does not exist';
+                state.locations[locationIndex].tasks = state.locations[locationIndex].tasks.filter((_, index) => index !== taskIndex);
+                state.error = null;
             }
         },
+        updateTask: (state, action: PayloadAction<{
+            locationName: string;
+            prevTaskName: string,
+            newTaskName: string
+        }>) => {
+            const [locationIndex, taskIndex, error] = findTask(state.locations, action.payload.locationName, action.payload.prevTaskName);
+            if (error) {
+                state.error = error;
+            } else {
+                // @ts-ignore
+                state.locations[locationIndex].tasks[taskIndex] = action.payload.newTaskName;
+                state.error = null;
+            }
+        },
+
         clearError: (state) => {
             state.error = null;
         },
     },
 });
 
-export const { addLocation, removeLocation, updateLocation, addTask, removeTask, clearError } = locationsSlice.actions;
+export const {
+    addLocation,
+    removeLocation,
+    updateLocation,
+    addTask,
+    removeTask,
+    updateTask,
+    clearError
+} = locationsSlice.actions;
 export default locationsSlice.reducer;
