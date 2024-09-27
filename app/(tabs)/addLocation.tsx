9f083348e30251314d781c2083b7dab8f0b0633a
Abdullah Saud <abdullahsaud03@gmail.com>
2024-09-27 19:51:33 +0500

    Implemented location tracking

--- app/(tabs)/addLocation.tsx
+++ app/(tabs)/addLocation.tsx
@@ -1,40 +1,61 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
-import {SafeAreaView, StyleSheet, View} from 'react-native';
+import {ActivityIndicator, SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import {GooglePlacesAutocomplete} from "react-native-google-places-autocomplete";
+import * as Location from 'expo-location';
 
 export default function App() {
     const [searchLocation, setSearchLocation] = React.useState<{ latitude: number; longitude: number } | null>(null);
+    const [currentLocation, setcurrentLocation] = useState<Location.LocationObject>();
+    const [errorMsg, setErrorMsg] = useState<string>();
+
+    useEffect(() => {
+        (async () => {
+            let {status} = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                setErrorMsg('Permission to access location was denied');
+                return;
+            }
+
+            let location = await Location.getCurrentPositionAsync({});
+            setcurrentLocation(location);
+        })();
+    }, []);
 
     return (
-        <SafeAreaView style={styles.container}>
-            <MapView
-                style={styles.map}
-                provider={PROVIDER_GOOGLE}
-                region={searchLocation ? {
-                    latitude: searchLocation.latitude,
-                    longitude: searchLocation.longitude,
-                    latitudeDelta: 0.001,
-                    longitudeDelta: 0.001
-                } : undefined}
-            />
-            <View style={styles.innerContainer}>
-                <GooglePlacesAutocomplete
-                    placeholder='Search'
-                    fetchDetails={true}
-                    onPress={(_, details) => {
-                        if (details) setSearchLocation({
-                            latitude: details.geometry.location.lat,
-                            longitude: details.geometry.location.lng
-                        });
-                    }}
-                    query={{
-                        key: 'AIzaSyC-QXb1OpjIBw8R9TPkZ3Tppr2qiayn2cg',
-                        language: 'en',
+        currentLocation ?
+            <SafeAreaView style={styles.container}>
+                <MapView
+                    style={styles.map}
+                    provider={PROVIDER_GOOGLE}
+                    region={{
+                        latitude: searchLocation ? searchLocation.latitude : currentLocation.coords.latitude,
+                        longitude: searchLocation ? searchLocation.longitude : currentLocation.coords.longitude,
+                        latitudeDelta: 0.001,
+                        longitudeDelta: 0.001
                     }}
                 />
-            </View>
-        </SafeAreaView>
+                <View style={styles.innerContainer}>
+                    <GooglePlacesAutocomplete
+                        placeholder='Search'
+                        fetchDetails={true}
+                        onPress={(_, details) => {
+                            if (details) setSearchLocation({
+                                latitude: details.geometry.location.lat,
+                                longitude: details.geometry.location.lng
+                            });
+                        }}
+                        query={{
+                            key: 'AIzaSyC-QXb1OpjIBw8R9TPkZ3Tppr2qiayn2cg',
+                            language: 'en',
+                        }}
+                    />
+                </View>
+            </SafeAreaView>
+            :
+            <SafeAreaView style={styles.center}>
+                <ActivityIndicator size={'large'}/>
+            </SafeAreaView>
     );
 }
 
@@ -51,5 +72,10 @@ const styles = StyleSheet.create({
         top: '10%',
         width: '94%',
         marginHorizontal: '3%',
+    },
+    center: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
     }
 });
