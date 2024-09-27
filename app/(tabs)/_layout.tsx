89c677a42985dff2b4e5bbe9bf6a88d6694d6cce
Abdullah Saud <abdullahsaud03@gmail.com>
2024-09-27 19:25:50 +0500

    Change from Stack navigation to Tabs navigation.

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,26 @@
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import {Tabs} from 'expo-router';
+
+export default function TabLayout() {
+    return (
+        <Tabs screenOptions={{tabBarActiveTintColor: 'blue'}}>
+            <Tabs.Screen
+                name="home"
+                options={{
+                    title: 'Home',
+                    tabBarIcon: ({color}) => <FontAwesome size={28} name="home" color={color}/>,
+                    headerShown: false
+                }}
+            />
+            <Tabs.Screen
+                name="addLocation"
+                options={{
+                    title: 'Add Location',
+                    tabBarIcon: ({color}) => <FontAwesome size={28} name="plus" color={color}/>,
+                    headerShown: false
+
+                }}
+            />
+        </Tabs>
+    );
+}
