962722725c946cb33fb9fcb66e9ec80f2d1fcba0
Abdullah Saud <abdullahsaud03@gmail.com>
2024-09-27 16:38:10 +0500

    Implement search for a specific location

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,13 @@
-import { Stack } from "expo-router";
+import {Stack} from "expo-router";
 import {Provider} from "react-redux";
 import {store} from "@/redux/store";
 
 export default function RootLayout() {
-  return (
-      <Provider store={store}>
-          <Stack>
-              <Stack.Screen name="index" />
-          </Stack>
-      </Provider>
-  );
+    return (
+        <Provider store={store}>
+            <Stack>
+                <Stack.Screen name="index" options={{headerShown: false}}/>
+            </Stack>
+        </Provider>
+    );
 }
