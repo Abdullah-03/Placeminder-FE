10828e9ea824abda6b1b1cc6a24c1ca57d4e1bee
Abdullah Saud <abdullahsaud03@gmail.com>
2024-10-02 17:40:49 +0500

    Refactor according to feedback

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,5 @@
-import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import HomeScreen from "@/screens/home";
 
 export default function Index() {
-    return (
-        <SafeAreaView style={styles.container}>
-            <Text>Home screen</Text>
-        </SafeAreaView>
-    );
+    return <HomeScreen />;
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    }
-});
---
+++ screens/home/index.tsx
@@ -0,0 +1,10 @@
+import {SafeAreaView, Text} from 'react-native';
+import styles from './styles'
+
+export default function HomeScreen() {
+    return (
+        <SafeAreaView style={styles.container}>
+            <Text>Home screen</Text>
+        </SafeAreaView>
+    );
+}
